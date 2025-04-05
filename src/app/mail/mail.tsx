7f418eb30a5f90e5b6cfb98e13f17694cece36da
iamnikhilranjan <nikhil21907@gmail.com>
2025-04-05 17:59:30 +0530

    mail-ui

--- src/app/mail/mail.tsx
+++ src/app/mail/mail.tsx
@@ -6,15 +6,30 @@ import { serializeJsonQuery } from "@prisma/client/runtime/library";
 
 type Props = {
     defaultLayout: number[] | undefined
+    navCollapseSize: number
 }
 
-const Mail = ({defaultLayout = [20, 32, 40]}: Props) => {
+const Mail = ({defaultLayout = [20, 32, 40], navCollapseSize}: Props) => {
     return(
         <TooltipProvider delayDuration={0}>
             <ResizablePanelGroup direction='horizontal' onLayout={(sizes: number[])=>{
                 console.log(sizes);
             }} className="items-stretch h-full min-h-screen">
+                <ResizablePanel defaultSize={defaultLayout[0]} collapsedSize={navCollapseSize} collapsible={true}
+                minSize={15}
+                maxSize={40}
+                onResize={() => {
 
+                }}
+                className={'min-w-[50px] transition-all duration-300 ease-in-out'}
+                >
+
+                    <div className="flex flex-col h-full flex-1">
+                        <div className="flex h-[52px] items-center justify-between">
+                            {/*Account switcher */}
+                        </div>
+                    </div>
+                </ResizablePanel>
             </ResizablePanelGroup>
         </TooltipProvider>
     )
